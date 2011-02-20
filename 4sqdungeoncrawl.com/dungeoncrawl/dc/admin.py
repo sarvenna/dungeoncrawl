169ac175b06f6a631f40bbcd514a40b15a9bd665
Jamie Culpon <jculpon@gmail.com>
2011-02-19 19:40:09 -0500

    Enables admin for Items, Monsters, and Category mapping

---
+++ 4sqdungeoncrawl.com/dungeoncrawl/dc/admin.py
@@ -0,0 +1,6 @@
+from dc.models import ItemType, MonsterType, CategoryAttribute
+from django.contrib import admin
+
+admin.site.register(ItemType)
+admin.site.register(MonsterType)
+admin.site.register(CategoryAttribute)
